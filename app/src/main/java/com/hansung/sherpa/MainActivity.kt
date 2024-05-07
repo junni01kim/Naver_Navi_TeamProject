bdac6be0148fa1dd9f7e27de5fe4b79d76719e20
KJH <kim03001@naver.com>
2024-05-08 02:00:40 +0900

    사용자 위치 마커 Error

--- app/src/main/java/com/hansung/sherpa/MainActivity.kt
+++ app/src/main/java/com/hansung/sherpa/MainActivity.kt
@@ -1,17 +1,70 @@
 package com.hansung.sherpa
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.LocationTrackingMode
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
 import com.naver.maps.map.NaverMapSdk
+import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.UiSettings
+import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.overlay.OverlayImage
+import com.naver.maps.map.util.FusedLocationSource
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var naverMap:NaverMap
+
+    private val LOCATION_PERMISSION_REQUEST_CODE = 1000
+    private lateinit var locationSource: FusedLocationSource
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         NaverMapSdk.getInstance(this).client =
             NaverMapSdk.NaverCloudPlatformClient(BuildConfig.CLIENT_ID) // 본인 api key
+
+        val fm = supportFragmentManager
+        val mapFragment = fm.findFragmentById(R.id.map) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(R.id.map, it).commit()
+            }
+
+        mapFragment.getMapAsync(this)
+
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>,
+                                            grantResults: IntArray) {
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions,
+                grantResults)) {
+            if (!locationSource.isActivated) { // 권한 거부됨
+                naverMap.locationTrackingMode = LocationTrackingMode.None
+            }
+            return
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onMapReady(p0: NaverMap){
+        this.naverMap = p0
+
+        naverMap.locationSource = locationSource
+        naverMap.uiSettings.setLocationButtonEnabled(true)
+
+        val locationOverlay = naverMap.locationOverlay
+        locationOverlay.isVisible = true
+
+        naverMap.addOnLocationChangeListener { location->
+            locationOverlay.position = LatLng(location.latitude,location.longitude)
+
+        }
+
     }
 }
