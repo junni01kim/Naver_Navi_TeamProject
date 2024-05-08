67e3a1368de435aeb6f6aea2ff4a77fc71a1bfc9
HS-JNYLee <2071013@hansung.ac.kr>
2024-05-09 08:57:02 +0900

    [refactor]: rename package name

--- app/src/main/java/com/hansung/sherpa/SearchRoute.kt
+++ app/src/main/java/com/hansung/sherpa/SearchRoute.kt
@@ -2,7 +2,7 @@ package com.hansung.sherpa
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.hansung.sherpa.Geocoding.GeocodingAPI
+import com.hansung.sherpa.geocoding.GeocodingAPI
 
 // 텍스트뷰에 값이 그래도 유지되고 있게하기 위한 뷰모델이다.
 // searchRouteViewModel의 이름과 위치는 바꿀 필요가 있을듯
