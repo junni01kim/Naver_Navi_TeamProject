f476e51968786fe48caa1cac67c4c7f9a7223f95
KJH <kim03001@naver.com>
2024-05-13 01:17:00 +0900

    RouteControl

---
+++ app/src/main/java/com/hansung/sherpa/deviation/RouteControl.kt
@@ -0,0 +1,12 @@
+package com.hansung.sherpa.deviation
+
+import com.naver.maps.geometry.LatLng
+import kotlin.collections.*
+
+class RouteControl constructor(route:MutableList<Pair<MutableList<LatLng>, String>>) {
+
+//    경로 이탈 : 10m
+//    경로 구간 확인 : 동적
+//    GPS 업데이트 시간 : 1.3s
+
+}
