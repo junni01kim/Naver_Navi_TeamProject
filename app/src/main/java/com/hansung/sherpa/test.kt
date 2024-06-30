dfb99579716fcd4db3e7043a37db0d8f0a904d09
KJH <kim03001@naver.com>
2024-07-01 00:24:33 +0900

    Pedestrian Route Request - TMAP

--- app/src/main/java/com/hansung/sherpa/test.kt
+++ app/src/main/java/com/hansung/sherpa/test.kt
@@ -5,10 +5,6 @@ import androidx.lifecycle.MutableLiveData
 import com.google.gson.Gson
 import com.hansung.sherpa.transit.PedestrianResponse
 import com.hansung.sherpa.transit.PedestrianRouteRequest
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
@@ -57,14 +53,32 @@ fun getPedestrianRoute(routeRequest: PedestrianRouteRequest): LiveData<Pedestria
     return resultLiveData
 }
 
+
+
 fun main(){
     val pr = PedestrianRouteRequest(
-        startX = "37.642636".toFloat(),
-        startY = "127.835763".toFloat(),
-        endX = "37.627448".toFloat(),
-        endY = "126.829388".toFloat()
+        startY = "37.642636".toFloat(),
+        startX = "127.835763".toFloat(),
+        endY = "37.627448".toFloat(),
+        endX = "126.829388".toFloat()
     )
 
     var res = getPedestrianRoute(pr)
+
+    /*
+    var res1 = TransitManager(StaticValue.mainActivity).getTransitRoutes(
+        TransitRouteRequest(
+            startX = "126.926493082645",
+            startY = "37.6134436427887",
+            endX = "127.126936754911",
+            endY = "37.5004198786564",
+            lang = 0,
+            format = "json",
+            count = 10
+        )
+    )
+    */
+
+
     println(res)
 }
