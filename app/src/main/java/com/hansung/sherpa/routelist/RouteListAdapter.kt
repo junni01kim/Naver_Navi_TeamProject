87e935bbffb86a8644421d2376f86fbb23bde4ae
junni01kim <audwnssp@naver.com>
2024-07-07 21:22:17 +0900

    add annotation

--- app/src/main/java/com/hansung/sherpa/routelist/RouteListAdapter.kt
+++ app/src/main/java/com/hansung/sherpa/routelist/RouteListAdapter.kt
@@ -18,9 +18,20 @@ data class Transport(var type:Int, var name: String, var remainingTime: String)
 
 /**
  * searchLocation에서 받아올 데이터 sample parameter
+ * 대중교통 단위로 나눠진 경로 리스트이다.
+ * 
+ * @param remainingTime 소요시간
+ * @param arrivalTime 도착시간
+ * @param isExpanded 세부사항 확장 여부
+ * @param transportList 대중교통 이용 정보(변경 가능)
  */
 data class RouteItem(val remainingTime: String, val arrivalTime: String, var isExpanded:Boolean, val transportList:List<Transport>)
 
+/**
+ * RouteListRecyclerView를 이용하기 위한 Adapter이다.
+ * @param itemList 출발지에서 목적지까지 이동할 수 잇는 경우의 수 리스트
+ * @param context
+ */
 class RouteListAdapter(val itemList: List<RouteItem>, val context: Context) :
     RecyclerView.Adapter<RouteListAdapter.ViewHolder>() {
 
@@ -43,6 +54,10 @@ class RouteListAdapter(val itemList: List<RouteItem>, val context: Context) :
         }
     }
 
+    /**
+     * 각각의 세부경로를 표현하기 위함
+     * @param transport 단일 대중교통 정보
+     */
     fun createLayout(transport: Transport) : View{
         val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         val layout = inflater.inflate(R.layout.expand_route_item, null) as LinearLayout
@@ -51,11 +66,12 @@ class RouteListAdapter(val itemList: List<RouteItem>, val context: Context) :
         val expandName = layout.findViewById<TextView>(R.id.expand_name)
         val expandRemainingTime = layout.findViewById<TextView>(R.id.expand_remaining_time)
 
-        val icon = when(transport.type){ // 샘플
+        // 타입 단위로 이미지를 설정한다.
+        val icon = when(transport.type){
             1 -> R.drawable.directions_bus
             2 -> R.drawable.train
             3 -> R.drawable.walk
-            else -> {R.drawable.walk}
+            else -> {R.drawable.cancel_widget} // 예외처리: 없는 타입
         }
         expandIcon.setImageResource(icon)
         expandName.text = transport.name
@@ -64,6 +80,12 @@ class RouteListAdapter(val itemList: List<RouteItem>, val context: Context) :
         return layout
     }
 
+    /**
+     * 세부 사항 확장 및 애니메이션 기능
+     * @param isExpanded 현재 세부 경로창이 확장되어 있는지 여부
+     * @param view
+     * @param layoutExpand 확장시킬 레이아웃 주체
+     */
     private fun toggleLayout(isExpanded: Boolean, view: View, layoutExpand:LinearLayout):Boolean{
         ToggleAnimation.toggleArrow(view, isExpanded)
         if(isExpanded){
@@ -73,6 +95,8 @@ class RouteListAdapter(val itemList: List<RouteItem>, val context: Context) :
         }
         return isExpanded
     }
+
+    // 이후는 일반적인 코드 로직이다.
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.route_item,parent,false))
