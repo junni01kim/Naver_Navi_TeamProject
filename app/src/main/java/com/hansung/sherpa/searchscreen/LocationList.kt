b46ed2b9918e6b4a106565e488d9deaf5f0b28f2
Myeongjun Kim <audwnssp@naver.com>
2024-08-05 01:52:05 +0900

    기본적인 디자인 완성

--- app/src/main/java/com/hansung/sherpa/searchscreen/LocationList.kt
+++ app/src/main/java/com/hansung/sherpa/searchscreen/LocationList.kt
@@ -1,8 +1,11 @@
 package com.hansung.sherpa.searchscreen
 
-import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
@@ -11,15 +14,14 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import com.hansung.sherpa.itemsetting.TransportRoute
 import com.hansung.sherpa.searchlocation.SearchLoactionCallBack
 import com.hansung.sherpa.searchlocation.SearchLocation
 import com.hansung.sherpa.searchlocation.SearchLocationResponse
-import kotlinx.coroutines.launch
 
 /**
  * 해야할 것
@@ -43,7 +45,15 @@ fun LocationList(locationValue:String, update: (String) -> Unit) {
 
     LazyColumn(verticalArrangement = Arrangement.spacedBy(2.dp)) {
         items(searchLocationResponse.items){
-            Text(text = it.title!! ,modifier = Modifier.fillMaxWidth())
+            Row(modifier = Modifier.background(Color.White).padding(vertical = 8.dp)){
+                Text(text = it.title!! , maxLines = 1)
+
+                Spacer(modifier = Modifier.weight(1f))
+
+                Text(text = it.address!!,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis)
+            }
         }
     }
 }
