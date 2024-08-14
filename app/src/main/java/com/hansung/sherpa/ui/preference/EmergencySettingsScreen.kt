9c70e7275c02a5eda3a95e60d5bfc70a1c9237bd
Myeongjun Kim <audwnssp@naver.com>
2024-08-15 02:37:09 +0900

    긴급연락처 디자인 완성

--- app/src/main/java/com/hansung/sherpa/ui/preference/EmergencySettingsScreen.kt
+++ app/src/main/java/com/hansung/sherpa/ui/preference/EmergencySettingsScreen.kt
@@ -1,10 +1,13 @@
 package com.hansung.sherpa.ui.preference
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -12,6 +15,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -85,7 +91,9 @@ fun EmergencySettingsScreen() {
         val caregiverUser1 = User1(1, "김명준", "010-9032-0000", "명준", caregiverUserSetting, caregiverUserAccount)
         val caregiverUserRelation = UserRelation(11, 1, 2, "보호자")
         item{
+            Divider("보호자 연락처")
             ItemRow(caregiverUserRelation.relation,caregiverUserAccount.address, caregiverUser1.telNum)
+            Divider("긴급 연락처")
         }
 
         // 이후로 긴급 연락처
@@ -102,8 +110,10 @@ fun EmergencySettingsScreen() {
 
         // 버튼 누를 시 전화하기 + 이동하기
         item {
+            Spacer(modifier = Modifier.height(2.dp))
             Row(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier.fillMaxWidth()
+                    .background(Color.White),
                 horizontalArrangement = Arrangement.End) {
                 Text("추가하기")
                 Spacer(modifier = Modifier.width(10.dp))
@@ -120,8 +130,21 @@ fun EmergencySettingsPreview() {
 
 @Composable
 fun ItemRow(name:String?, address:String?, telNum:String?) {
-    Row(verticalAlignment = Alignment.CenterVertically ) {
-        Spacer(modifier = Modifier.width(10.dp))
+    Row(verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.White)
+            .drawBehind {
+                val strokeWidth = 1 * density
+                val y = size.height - strokeWidth / 2
+                drawLine(
+                    Color(240, 240, 240),
+                    Offset(0f, y),
+                    Offset(size.width, y),
+                    strokeWidth
+                )
+            }
+            .padding(horizontal = 12.dp, vertical = 12.dp)) {
         Text(
             text = name?:"None",
             modifier = Modifier.width(80.dp),
@@ -130,17 +153,16 @@ fun ItemRow(name:String?, address:String?, telNum:String?) {
         )
         Text(
             text = address?:"None",
-            modifier = Modifier.width(200.dp),
+            modifier = Modifier.width(185.dp),
             maxLines = 1,
             overflow = TextOverflow.Ellipsis
         )
         Spacer(modifier = Modifier.weight(1f))
         Text(
             text = telNum?:"None",
-            modifier = Modifier.width(100.dp),
+            modifier = Modifier.width(95.dp),
             maxLines = 1,
             overflow = TextOverflow.Ellipsis
         )
-        Spacer(modifier = Modifier.width(10.dp))
     }
 }
