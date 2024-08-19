e273084eb4e2abe7e51d30644970a77e8fb9eb1f
Myeongjun Kim <audwnssp@naver.com>
2024-08-19 23:18:56 +0900

    navController 시작 지점 변경

--- app/src/main/java/com/hansung/sherpa/MainActivity.kt
+++ app/src/main/java/com/hansung/sherpa/MainActivity.kt
@@ -121,7 +121,7 @@ class MainActivity : ComponentActivity(), OnMapReadyCallback {
                     val navController = rememberNavController()
                     NavHost(
                         navController = navController,
-                        startDestination = SherpaScreen.Preference.name
+                        startDestination = SherpaScreen.Start.name
                     ){
                         composable(route = SherpaScreen.Start.name){
                             StartScreen(navController, Modifier.padding(innerPadding))
