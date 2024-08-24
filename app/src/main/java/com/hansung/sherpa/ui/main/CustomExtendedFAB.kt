5c62c691e5686ea831e7391c65a11a8de0862219
HS-JNYLee <2071013@hansung.ac.kr>
2024-08-24 11:50:17 +0900

    [debug]: emergency fab area distract map drag issue

--- app/src/main/java/com/hansung/sherpa/ui/main/CustomExtendedFAB.kt
+++ app/src/main/java/com/hansung/sherpa/ui/main/CustomExtendedFAB.kt
@@ -9,20 +9,16 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.animation.shrinkHorizontally
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
@@ -32,10 +28,10 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -46,7 +42,6 @@ import androidx.compose.ui.semantics.clearAndSetSemantics
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hansung.sherpa.R
-import com.hansung.sherpa.transit.pedestrian.PedestrianResponse
 
 private val CustomIconWidth = 33.dp
 
@@ -58,7 +53,7 @@ private val ExtendedFabEndIconPadding = 12.dp
 
 private val ExtendedFabTextPadding = 20.dp
 
-private val ExtendedFabMinimumWidth = 80.dp
+private val ExtendedFabMinimumWidth = 40.dp
 
 sealed class IconType {
     data class Vector(val imageVector: ImageVector) : IconType()
@@ -194,20 +189,17 @@ fun ExtendedFABContainer(isVisible: Boolean = true) {
     val visibleList = remember {
         List(tripleEmrgList.size) { mutableStateOf(false) }
     }
-    Column(
-        modifier = Modifier
-            .wrapContentSize()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.End
+    if (isVisible) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.End
 
-    ) {
-        if (isVisible) {
+        ) {
             LazyColumn(
                 verticalArrangement = Arrangement.spacedBy(8.dp),
-                horizontalAlignment = Alignment.End,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .wrapContentHeight()
+                modifier = Modifier.wrapContentHeight()
             ) {
                 items(tripleEmrgList.size) { index ->
                     val item = tripleEmrgList[index]
