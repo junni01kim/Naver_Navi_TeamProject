65c0e260af2cda50e592e374293f6f49046d6172
HS-JNYLee <2071013@hansung.ac.kr>
2024-09-01 11:06:20 +0900

    [debug]: rolllback DrawPathOverlay.kt

--- app/src/main/java/com/hansung/sherpa/ui/specificroute/DrawPathOverlay.kt
+++ app/src/main/java/com/hansung/sherpa/ui/specificroute/DrawPathOverlay.kt
@@ -15,7 +15,7 @@ import com.naver.maps.map.overlay.OverlayImage
 /**
  * NaverMap에 대중교통 경로를 그리는 Compose
  * ※ 무조건 NaverMap compose에서만 사용해야 한다.
- * 
+ *
  * @param transportRoute SearchScreen에서 가져온 대중교통 경로
  */
 @Composable
@@ -41,16 +41,7 @@ fun DrawPathOverlay(
 fun setCoordParts(transportRoute: TransportRoute): SnapshotStateList<MutableList<LatLng>> {
     val coordParts = mutableStateListOf<MutableList<LatLng>>()
 
-    transportRoute.subPath.forEachIndexed {
-        index, subPath ->
-        coordParts.add(subPath.sectionRoute.routeList)
-        if(index != transportRoute.subPath.size - 1) {
-            val next = transportRoute.subPath[index + 1].sectionRoute.routeList
-            val curr = transportRoute.subPath[index].sectionRoute.routeList
-            val list = listOf<LatLng>(curr[curr.size-1], next[0])
-            coordParts.add(list.toMutableList())
-        }
-    }
+    for(subPath in transportRoute.subPath) coordParts.add(subPath.sectionRoute.routeList)
 
     return coordParts
 }
@@ -58,23 +49,13 @@ fun setCoordParts(transportRoute: TransportRoute): SnapshotStateList<MutableList
 fun setColerParts(transportRoute: TransportRoute): MutableList<ColorPart> {
     val colorParts = mutableListOf<ColorPart>()
 
-    transportRoute.subPath.forEachIndexed {
-        index, subPath ->
+    for(subPath in transportRoute.subPath) {
         colorParts.add(ColorPart(
             color = if(subPath.trafficType == 3) Color.Gray else typeOfColor(subPath),
             outlineColor = Color.Transparent,
             passedColor = Color.Transparent,
             passedOutlineColor = Color.Transparent
         ))
-        if(index != transportRoute.subPath.size - 1) {
-            colorParts.add(ColorPart(
-                color = Color.Gray,
-                outlineColor = Color.Transparent,
-                passedColor = Color.Transparent,
-                passedOutlineColor = Color.Transparent
-            ))
-        }
-
     }
 
     return colorParts
