5621631cddfde93e89833cc35f439ec2d7081c33
junni01kim <audwnssp@naver.com>
2024-09-05 02:36:57 +0900

    caregiverViewModel 디코딩 영역 분리 사용하지 않는 공공데이터포털 버스 도착 예정시간 삭제

--- app/src/main/java/com/hansung/sherpa/arrivalinfo/ArrivalInfoManager.kt
+++ app/src/main/java/com/hansung/sherpa/arrivalinfo/ArrivalInfoManager.kt
@@ -2,9 +2,6 @@ package com.hansung.sherpa.arrivalinfo
 
 import android.util.Log
 import com.google.gson.Gson
-import com.hansung.sherpa.arrivalinfo.common.ArrivalInfoRequest
-import com.hansung.sherpa.arrivalinfo.common.ArrivalInfoResponse
-import com.hansung.sherpa.arrivalinfo.common.ArrivalInfoService
 import com.hansung.sherpa.arrivalinfo.odsay.ODsayArrivalInfoRequest
 import com.hansung.sherpa.arrivalinfo.odsay.ODsayArrivalInfoResponse
 import com.hansung.sherpa.arrivalinfo.odsay.ODsayArrivalInfoService
@@ -16,32 +13,6 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.io.IOException
 
 class ArrivalInfoManager {
-    /**
-     * 정류소별특정노선버스 도착예정정보 목록조회 API를 사용해 경로 데이터를 가져와 역직렬화하는 함수
-     *
-     * @param request 요청할 정보 객체
-     * @return BusArrivalInfoResponse
-     */
-    fun getArrivalInfoList(request: ArrivalInfoRequest): ArrivalInfoResponse? {
-        var result: ArrivalInfoResponse? = null
-        runBlocking {
-            launch(Dispatchers.IO) {
-                try {
-                    val response = Retrofit.Builder()
-                        .baseUrl("https://apis.data.go.kr/1613000/ArvlInfoInqireService/")
-                        .addConverterFactory(GsonConverterFactory.create())
-                        .build()
-                        .create(ArrivalInfoService::class.java)
-                        .getService(request.getMap()).execute()
-                    result = Gson().fromJson(response.body()!!.string(), ArrivalInfoResponse::class.java)
-                } catch (e: IOException) {
-                    Log.e("API Log: IOException", "getArrivalInfoList: ${e.message}(e.message)")
-                }
-            }
-        }
-        return result
-    }
-
     /**
      * 정류소 별 특정 노선 버스 도착 예정 정보, 목록 조회 API를 사용해 경로 데이터를 가져와 역 직렬화하는 함수
      *
